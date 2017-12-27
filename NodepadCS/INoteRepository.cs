d0949060a95c8ac98cc474a7c20625be03d36133
Phil Pursglove <philpursglove@users.noreply.github.com>
2017-12-27 22:32:01 +0000

    Repo interface for notes

---
+++ NodepadCS/INoteRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using NodepadCS.Models;
+
+namespace NodepadCS
+{
+    public interface INoteRepository
+    {
+        List<Note> GetAll();
+        Note Get(string id);
+        void Create(Note note);
+        void Update(Note note);
+
+    }
+}
