f9791d2f97bc27628d1183b697284ad4b586c312
Phil Pursglove <philpursglove@users.noreply.github.com>
2017-12-27 22:31:39 +0000

    Controller for Notes!

---
+++ NodepadCS/Controllers/NotesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NodepadCS.Controllers
+{
+    public class NotesController : Controller
+    {
+        // GET: Notes
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
