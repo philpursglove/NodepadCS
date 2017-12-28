ee7d288b53a8f98a7068453cdf66a1aba28dbb48
Phil Pursglove <philpursglove@users.noreply.github.com>
2017-12-28 10:27:50 +0000

    MongoNoteRepo - WIP

---
+++ NodepadCS/MongoNoteRepository.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using MongoDB.Driver;
+using NodepadCS.Models;
+
+namespace NodepadCS
+{
+    public class MongoNoteRepository : INoteRepository
+    {
+        private MongoClient client;
+
+
+        public MongoNoteRepository()
+        {
+            client = new MongoClient("mongodb://localhost/nodepad");
+        }
+        public void Create(Note note)
+        {
+            IMongoDatabase db = client.GetDatabase("nodepad");
+            IMongoCollection<Note> collection = db.GetCollection<Note>("notes");
+
+            collection.InsertOne(note);
+        }
+
+        public Note Get(string id)
+        {
+            IMongoDatabase db = client.GetDatabase("nodepad");
+            IMongoCollection<Note> collection = db.GetCollection<Note>("notes");
+
+            return collection.FindAsync(n => n.Id == id).Result;
+        }
+
+        public List<Note> GetAll()
+        {
+            IMongoDatabase db = client.GetDatabase("nodepad");
+            IMongoCollection<Note> collection = db.GetCollection<Note>("notes");
+
+            return collection.FindAsync().Result.ToList();
+        }
+
+        public void Update(Note note)
+        {
+            IMongoDatabase db = client.GetDatabase("nodepad");
+            IMongoCollection<Note> collection = db.GetCollection<Note>("notes");
+
+            collection.UpdateOne(note);
+        }
+    }
+}
