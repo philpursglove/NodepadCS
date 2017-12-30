3bd7376006c7de144247e23525d1818c110f776a
Phil Pursglove <philpursglove@users.noreply.github.com>
2017-12-30 00:11:41 +0000

    Initial wiring for repository

--- NodepadCS/Controllers/NotesController.cs
+++ NodepadCS/Controllers/NotesController.cs
@@ -8,6 +8,13 @@ namespace NodepadCS.Controllers
 {
     public class NotesController : Controller
     {
+        private readonly INoteRepository noteRepository;
+
+        public NotesController(INoteRepository noteRepository)
+        {
+            this.noteRepository = noteRepository;
+        }
+
         // GET: Notes
         public ActionResult Index()
         {
